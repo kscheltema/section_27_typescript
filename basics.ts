4e2d84c77cb26971f7d36c2c44f9240b0ca85774
kscheltema <k87scheltema@outlook.com>
2021-07-16 13:59:58 +0200

    primitive types

---
+++ basics.ts
@@ -0,0 +1,10 @@
+//primitives: number, boolean, string
+//more complex: arrays, objects
+//function types & parameters
+
+//primitives
+let age: number = 12; //'12' would give an error - Number would give an error
+let userName: string = 'Max';
+let isInstructor: boolean;
+
+isInstructor = true;
