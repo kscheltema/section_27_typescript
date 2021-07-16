bffe6e3280a85c248924f1f502d3be4767b5a107
kscheltema <k87scheltema@outlook.com>
2021-07-16 16:56:54 +0200

    gereric description - felxibility with safegaurds

--- basics.ts
+++ basics.ts
@@ -45,3 +45,17 @@ function print(value: any) {
   console.log(value); //special return value assigned - void aka no return
 }
 
+//generics
+//can convert a function so that it adopts different type values
+// <T> tells JS that both parameters should be of the same type
+// but can be called a number of times with a different set of types each time
+function insertAtBeginning<T>(array: T[], value: T){
+  const newArr = [value, ...array];
+  return newArr;
+}
+
+const demoArray = [1,2,3];
+
+const updatedArray = insertAtBeginning(demoArray, -1);    //-1,1,2,3
+const stringArray = insertAtBeginning(['a','b','c'],'d'); //a, b, c, d
+
